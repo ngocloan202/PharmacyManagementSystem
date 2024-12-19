972b2244c731695a13e723716be627de8b7be49f
Loan Chau <chauthingocloan0309@gmail.com>
2024-12-19 10:33:31 +0700

    profile setting form

--- PharmacyManagement/Program.cs
+++ PharmacyManagement/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PharmacyManagement.View;
 
 namespace PharmacyManagement
 {
@@ -16,7 +17,7 @@ namespace PharmacyManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new Profile());
         }
     }
 }
