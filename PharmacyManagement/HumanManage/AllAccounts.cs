90e3a383571261ae8f8deebbb2ede5c0e696cf6f
Loan Chau <chauthingocloan0309@gmail.com>
2024-12-22 09:51:27 +0700

    add basic infor

--- PharmacyManagement/HumanManage/AllAccounts.cs
+++ PharmacyManagement/HumanManage/AllAccounts.cs
@@ -6,10 +6,10 @@ using PharmacyManagement.DB_query;
 
 namespace PharmacyManagement.HumanManage
 {
-    public partial class AllUsers : XtraForm
+    public partial class AllAccounts : XtraForm
     {
         PharmacyMgtDatabase dataTable = new PharmacyMgtDatabase();
-        public AllUsers()
+        public AllAccounts()
         {
             dataTable.OpenConnection();
             InitializeComponent();
@@ -17,8 +17,8 @@ namespace PharmacyManagement.HumanManage
 
         public void getData()
         {
-            string userSql = @"SELECT ac.AccountID, ac.Username, em.EmployeeName, ac.UserRole,
-                               em.Sex, em.Contact, em.Birthday, em.EmployeeAddress
+            string userSql = @"SELECT ac.AccountID, ac.Username, ac.UserRole, 
+                                      ac.UserPassword ,em.EmployeeName
                                FROM ACCOUNT as ac, EMPLOYEE as em
                                WHERE ac.EmployeeID = em.EmployeeID";
             SqlCommand userCmd = new SqlCommand(userSql);
@@ -26,13 +26,13 @@ namespace PharmacyManagement.HumanManage
             BindingSource binding = new BindingSource();
             binding.DataSource = dataTable;
 
-            dgvAllUsers.DataSource = binding;
+            dgvAllAccounts.DataSource = binding;
             bindingNavigator.BindingSource = binding;
         }
 
         private void AllUsers_Load(object sender, EventArgs e)
         {
-            dgvAllUsers.AutoGenerateColumns = false;
+            dgvAllAccounts.AutoGenerateColumns = false;
             getData();
         }
     }
