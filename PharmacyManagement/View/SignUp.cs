ef5ef0902b190fde16512e8249decca6d2c465d9
Nguyen Quan <ngquan6704@gmail.com>
2024-12-22 08:12:37 +0700

    validate input Sign Up form

--- PharmacyManagement/View/SignUp.cs
+++ PharmacyManagement/View/SignUp.cs
@@ -31,6 +31,21 @@ namespace PharmacyManagement
 
         private void btnSignUp_Click(object sender, EventArgs e)
         {
+            if (!validateInput())
+            {
+                return;
+            }
+            SignUp signUp = new SignUp();
+            if (!signUp.IsDisposed)
+            {
+                this.Hide();
+                SignIn signIn = new SignIn();
+                signIn.ShowDialog();
+            }
+        }
+
+        public Boolean validateInput()
+        {
             ToolTip toolTip = new ToolTip();
             toolTip.IsBalloon = true;
 
@@ -39,19 +54,30 @@ namespace PharmacyManagement
                 toolTip.Show("Please fill your username", txtUsername,
                     txtUsername.Width - 15, txtUsername.Height - 80, 2000);
                 txtUsername.Focus();
+                return false;
             }
             else if (txtPassword.Text.Trim() == "")
             {
                 toolTip.Show("Please fill your password", txtPassword,
                     txtPassword.Width - 15, txtPassword.Height - 80, 2000);
                 txtPassword.Focus();
+                return false;
             }
             else if (txtConfirmPass.Text.Trim() == "")
             {
                 toolTip.Show("Please fill your confirm password", txtConfirmPass,
                     txtConfirmPass.Width - 15, txtConfirmPass.Height - 80, 2000);
                 txtConfirmPass.Focus();
+                return false;
             }
+            else if (txtPassword.Text.Trim() != txtConfirmPass.Text.Trim() == true)
+            {
+                toolTip.Show("Confirm password is incorrect!", txtConfirmPass,
+                    txtConfirmPass.Width - 15, txtConfirmPass.Height - 80, 2000);
+                txtConfirmPass.Focus();
+                return false;
+            }
+            return true;
         }
     }
 }
