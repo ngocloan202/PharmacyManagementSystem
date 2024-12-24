4d22b07b8cae96f4952fad54bcb69be128c248b5
Nguyen Quan <ngquan6704@gmail.com>
2024-12-24 17:44:27 +0700

    remove the lib is redundant

--- PharmacyManagement/HumanManage/NewCustomer.cs
+++ PharmacyManagement/HumanManage/NewCustomer.cs
@@ -1,15 +1,10 @@
 ﻿using DevExpress.XtraEditors;
 using PharmacyManagement.DB_query;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PharmacyManagement.HumanManage
